54aef54766c0faea484d9a773aa3b54a4d86c549
GrassSand <grasssand@gmail.com>
2021-08-27 22:08:54 +0800

    Fixed fetch_endpoint with chinese accounts (#30)
    Previously genshinstats would not work with Chinese accounts due to an invalid ds salt.

--- genshinstats/hoyolab.py
+++ genshinstats/hoyolab.py
@@ -34,13 +34,14 @@ def get_langs() -> Dict[str, str]:
     )['langs']
     return {i['value']: i['name'] for i in data}
 
-def search(keyword: str, size: int = 20) -> List[Dict[str, Any]]:
+def search(keyword: str, size: int = 20, chinese: bool = False) -> List[Dict[str, Any]]:
     """Searches all users.
 
     Can return up to 20 results, based on size.
     """
+    url = "https://bbs-api.mihoyo.com/" if chinese else "https://api-os-takumi.mihoyo.com/community/"
     return fetch_endpoint(
-        "community/apihub/wapi/search",
+        url + "apihub/wapi/search",
         cookie={}, # this endpoint does not require cookies
         params=dict(keyword=keyword, size=size, gids=2)
     )['users']
@@ -50,19 +51,21 @@ def set_visibility(public: bool, cookie: Mapping[str, Any] = None) -> None:
     fetch_endpoint(
         "game_record/card/wapi/publishGameRecord",
         cookie=cookie,
-        method='POST', 
+        method='POST',
         # what's game_id about???
         json=dict(is_public=public, game_id=2)
     )
 
 @deprecated()
-def hoyolab_check_in(cookie: Mapping[str, Any] = None) -> None:
+def hoyolab_check_in(chinese: bool = False, cookie: Mapping[str, Any] = None) -> None:
     """Checks in the currently logged-in user to hoyolab.
 
     This function will not claim daily rewards!!!
     """
+    url = "https://bbs-api.mihoyo.com/" if chinese else "https://api-os-takumi.mihoyo.com/community/"
     fetch_endpoint(
-        "community/apihub/api/signIn",
+        url + "apihub/api/signIn",
+        chinese=chinese,
         cookie=cookie,
         method='POST',
         json=dict(gids=2)
@@ -77,7 +80,7 @@ def get_game_accounts(chinese: bool = False, cookie: Mapping[str, Any] = None) -
     data = fetch_endpoint(url+"binding/api/getUserGameRolesByCookie", cookie=cookie)['list']
     return prettify_game_accounts(data)
 
-def get_record_card(hoyolab_uid: int, cookie: Mapping[str, Any] = None) -> Optional[Dict[str, Any]]:
+def get_record_card(hoyolab_uid: int, chinese: bool = False, cookie: Mapping[str, Any] = None) -> Optional[Dict[str, Any]]:
     """Gets a game record card of a user based on their hoyolab uid.
 
     A record card contains data regarding the stats of a user for their displayed server.
@@ -88,18 +91,19 @@ def get_record_card(hoyolab_uid: int, cookie: Mapping[str, Any] = None) -> Optio
     """
     cards = fetch_endpoint(
         "game_record/card/wapi/getGameRecordCard",
+        chinese=chinese,
         cookie=cookie,
         params=dict(uid=hoyolab_uid, gids=2)
     )['list']
     return cards[0] if cards else None
 
-def get_uid_from_hoyolab_uid(hoyolab_uid: int, cookie: Mapping[str, Any] = None) -> Optional[int]:
+def get_uid_from_hoyolab_uid(hoyolab_uid: int, chinese: bool = False, cookie: Mapping[str, Any] = None) -> Optional[int]:
     """Gets a uid with a community uid.
 
     This is so it's possible to search a user and then directly get the uid.
     In case the uid is private, returns None.
     """
-    card = get_record_card(hoyolab_uid, cookie)
+    card = get_record_card(hoyolab_uid, chinese, cookie)
     return int(card['game_role_id']) if card else None
 
 def redeem_code(code: str, uid: int = None, cookie: Mapping[str, Any] = None) -> None:
@@ -108,12 +112,12 @@ def redeem_code(code: str, uid: int = None, cookie: Mapping[str, Any] = None) ->
     Api endpoint for https://genshin.mihoyo.com/en/gift.
     !!! This function requires account_id and cookie_token cookies !!!
 
-    The code will be redeemed for every avalible account, 
+    The code will be redeemed for every avalible account,
     specifying the uid will claim it only for that account.
     Returns the amount of users it managed to claim codes for.
 
-    You can claim codes only every 5s so you must sleep between claims. 
-    The function sleeps for you when claiming for every account 
+    You can claim codes only every 5s so you must sleep between claims.
+    The function sleeps for you when claiming for every account
     but you must sleep yourself when passing in a uid or when an error is encountered.
 
     Currently codes can only be claimed for overseas accounts, not chinese.
@@ -127,7 +131,7 @@ def redeem_code(code: str, uid: int = None, cookie: Mapping[str, Any] = None) ->
         )
     else:
         # cannot claim codes for accounts with ar lower than 10
-        accounts = [account for account in get_game_accounts(cookie=cookie) 
+        accounts = [account for account in get_game_accounts(cookie=cookie)
                     if account['level'] >= 10]
         for i, account in enumerate(accounts):
             if i: time.sleep(5) # there's a ratelimit of 1 request every 5 seconds
@@ -143,7 +147,7 @@ def get_recommended_users(page_size: int = None) -> List[Dict[str, Any]]:
 
 def get_hot_posts(forum_id: int = 1, size: int = 100, lang: str = 'en-us') -> List[Dict[str, Any]]:
     """Fetches hot posts from the front page of hoyolabs
-    
+
     Posts are split into different forums set by ids 1-5.
     There may be less posts returned than size.
     """
